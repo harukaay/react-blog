2f2f5d2bff44dcfacb2e93c8cabf649cb7e3f095
邓亮 <dl@cyhzsoft.com>
2020-04-10 09:39:55 +0800

    '后台项目初始化'

---
+++ admin/src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./Login";
+
+function Main() {
+  return (
+    <div>
+      <Router>
+        <Route path="/login/" exact component={Login} />
+      </Router>
+    </div>
+  );
+}
+
+export default Main;
